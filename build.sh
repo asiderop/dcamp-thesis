64a3216fe6cecbfff7f320b17af39f9761c78c18
Alexander Sideropoulos <alexander@thequery.net>
2014-05-03 21:08:18 -0700

    use runall.sh in build script

--- build.sh
+++ build.sh
@@ -10,7 +10,4 @@ then
 	rm out/*
 fi
 
-pdflatex -output-directory out thesis
-bibtex out/thesis
-pdflatex -output-directory out thesis
-pdflatex -output-directory out thesis
+runall.sh 'pdflatex -output-directory out thesis' 'bibtex out/thesis' 'pdflatex -output-directory out thesis' 'pdflatex -output-directory out thesis'
